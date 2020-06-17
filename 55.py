48ba7377f859b7fa0fa3b7314545e76da336bf7a
Determined8989 <67053519+Determined8989@users.noreply.github.com>
2020-06-17 16:39:44 +0300

    functions

--- 55.py
+++ 55.py
@@ -1,20 +1,31 @@
 import os 
 import datetime
-while True: 
-	a = input('Введите слово:')
-	if a.lower() == "exit":
-		basedir = "LOG"
-		subnames = os.listdir(basedir)
-		print(len(subnames))
-		sub_size = 0
-		for subname in subnames:
-			sub_size += os.path.getsize('LOG/'+ subname)		    
-		print (sub_size)
-		break
-	elif a.lower() == "clear":
-		os.system('cls||clear')
-	else:
-		p = datetime.datetime.now()
-		f = open('LOG/log_' + p.strftime("%Y%m%d%H%M") + '.txt', 'a') 	
-		f.write (p.strftime("%Y-%m-%d-%H.%M.%S") + '-' + a + '\n')
-		f.close()
+def SaveLog(s):
+	p = datetime.datetime.now()
+	f = open('LOG/log_' + p.strftime("%Y%m%d%H%M") + '.txt', 'a') 	
+	f.write (p.strftime("%Y-%m-%d-%H.%M.%S") + '-' + s + '\n')
+	f.close()	
+def printInfo():
+	basedir = "LOG"
+	subnames = os.listdir(basedir)
+	print(len(subnames))
+	sub_size = 0
+	for subname in subnames:
+		sub_size += os.path.getsize('LOG/'+ subname)		    
+	print (sub_size)
+def cls():
+	os.system('cls||clear')
+def main():
+	SaveLog("Пользователь начал работу")
+	while True: 
+		a = input('Введите слово:')
+		if a.lower() == "exit":
+			printInfo()
+			SaveLog('Пользователь завершил работу')
+			break
+		elif a.lower() == "clear":
+			cls()
+			SaveLog ('Пользователь очистил экран')
+		else:
+			SaveLog ('Пользователь ввел неизвестную команду: ' + a)
+main()
